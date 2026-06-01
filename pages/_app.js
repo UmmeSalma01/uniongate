ea6a59925b3af98e87e4c0bb7c76aa1e0afff665
UmmeSalma01 <10c51ummesalma@gmail.com>
2026-06-01 17:09:31 +0530

    Add Google Search Console verification

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion'
 import ScrollProvider from '../components/ScrollProvider'
 import Loader from '../components/Loader'
 import { useState, useEffect } from 'react'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   const [loading, setLoading] = useState(true)
@@ -13,14 +14,27 @@ export default function App({ Component, pageProps }) {
   }, [])
 
   return (
-    <ScrollProvider>
-      {loading ? (
-        <Loader />
-      ) : (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-          <Component {...pageProps} />
-        </motion.div>
-      )}
-    </ScrollProvider>
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="EtS30W-3FtizCU2VQatLbObfBOkR1_i1xMn55YOeyWI"
+        />
+      </Head>
+
+      <ScrollProvider>
+        {loading ? (
+          <Loader />
+        ) : (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        )}
+      </ScrollProvider>
+    </>
   )
 }
