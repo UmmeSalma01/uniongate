820c1ae407511c4b06ce668d140ba3dceb01c090
UmmeSalma01 <10c51ummesalma@gmail.com>
2026-06-01 17:45:27 +0530

    Add Google Search Console verification meta tag

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="EtS30W-3FtizCU2VQatLbObfBOkR1_i1xMn55YOeyWI"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
